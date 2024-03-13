3f0f897aedb12b3af30f7e8cd4d449c825b58166
tinuola <24995224+tinuola@users.noreply.github.com>
2024-03-12 21:52:38 -0700

    feat: add input field and basic display logic

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -1,7 +1,24 @@
 import PropTypes from 'prop-types'
+import iconEllipsis from '../assets/images/icon-ellipsis.svg'
 import verifyStorage from '../utils/verifyStorage'
 
 function StatCard({ currPeriod, prevPeriod, stats }) {
+  const showEditField = (idx) => {
+    console.log('Display an input field!')
+    document.querySelectorAll('form')[idx].classList.toggle('form')
+  }
+
+  const getUpdatedDailyValue = (idx) => {
+    let inputFields = document.querySelectorAll('input')
+    if (inputFields[idx].value) {
+      console.log('Updated value is: ', inputFields[idx].value)
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <>
       <h3>StatCard</h3>
@@ -18,13 +35,32 @@ function StatCard({ currPeriod, prevPeriod, stats }) {
                 {stat.prevStat > 1 && `s`}
               </span>
             </p>
+            {/* Display edit icon only if selection is 'Daily' and browser accepts storage */}
             {currPeriod === 'daily' && verifyStorage('localStorage') && (
               <>
-                <p>Yes, there's storage </p>
-                <img
-                  src=''
-                  alt=''
-                />
+                <span>Edit </span>
+                <button onClick={() => showEditField(index)}>
+                  <img
+                    src={iconEllipsis}
+                    alt=''
+                  />
+                </button>
+                <form
+                  onSubmit={handleSubmit}
+                  className='form'
+                >
+                  <input
+                    type='number'
+                    name=''
+                    id=''
+                    className='input'
+                  />
+                  <button onClick={() => getUpdatedDailyValue(index)}>
+                    Update
+                  </button>
+                </form>
+                <br />
+                <br />
               </>
             )}
           </li>
