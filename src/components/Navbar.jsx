744fddb9079062ca870b22dbaf8baed28bec4a8a
tinuola <24995224+tinuola@users.noreply.github.com>
2024-03-19 23:59:02 -0700

    refactor: renaming theme colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import { FaMoon, FaSun } from 'react-icons/fa'
 
 function Navbar() {
+  // Theme switching:
+  // https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/#aa-using-a-body-class
+
   const prefersLightScheme = window.matchMedia('(prefers-color-scheme: light)')
 
   const handleThemeSwitch = (e) => {
     e.target.classList.toggle('toggle')
-    console.log(e.target.classList)
 
-    // If the OS is set to dark mode...
+    // If the OS is set to light mode...
     if (prefersLightScheme.matches) {
-      // ...then apply the .light-theme class to override those styles
+      // ...then apply the dark theme to override those styles
       document.body.classList.toggle('theme-dark')
       // Otherwise...
     } else {
-      // ...apply the .dark-theme class to override the default light styles
+      // ...apply the light theme to override default (dark) styles
       document.body.classList.toggle('theme-light')
     }
   }
@@ -29,13 +31,15 @@ function Navbar() {
         </a> */}
         <h1 className='sr-only'>Project Name - A Frontend Mentor Project</h1>
       </header>
-      <div className='theme-toggle'>
-        <FaSun className='light' />
-        <FaMoon className='dark' />
-        <button
-          onClick={handleThemeSwitch}
-          className='toggle-control'
-        ></button>
+      <div className='toggle-wrapper'>
+        <div className='theme-toggle'>
+          <FaSun className='light' />
+          <FaMoon className='dark' />
+          <button
+            onClick={handleThemeSwitch}
+            className='toggle-control'
+          ></button>
+        </div>
       </div>
     </nav>
   )
